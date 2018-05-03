a8997a9b759d46b1cb59f3e44f08e7d52cd52acf
nareshkumaar <nareshkumar1845@gmail.com>
2018-05-03 16:40:57 -0400

    Text validation in Page object

--- src/com/testng/po/ultimateqa.java
+++ src/com/testng/po/ultimateqa.java
@@ -19,34 +19,38 @@ public class ultimateqa {
 
 	public static WebDriver driver;
 	public Logger logger;
-	
+
 	@BeforeTest
-	public static void browserSetup() {
+	public void browserSetup() {
+		logger = Logger.getLogger("ultimateqa");
+		PropertyConfigurator.configure("log4j.properties");
+		
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\prokarma\\eclipse-workspace\\SampleAutomation\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.ultimateqa.com/simple-html-elements-for-automation/");
 		driver.manage().window().maximize();
-		System.out.println("Selenium Webdriver Script in Chrome browser");
+		logger.info("Selenium Webdriver Script in Chrome browser");
 	}
 
 	@BeforeTest
 	public void loggerSetup() {
-		logger = Logger.getLogger("ultimateqa");
-		PropertyConfigurator.configure("log4j.properties");
+		
 	}
 
-	@Test
+	@Test(priority=1)
 	public void buttonClick1() {
-		
+		/*
+		 * Button click and validate the text in next page
+		 */
 		driver.findElement(By.xpath("//a[@class='et_pb_promo_button et_pb_button']")).click();
 		logger.info("Button clicked");
 
 		String text = "Button success";
-		List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + text + "')]"));
-		System.out.println(list.get(0).getText());
+		List<WebElement> list = driver.findElements(By.xpath("//h1[contains(text(),'" + text + "')]"));
+		System.out.println(list.get(0).getText() + ": Text Validated");
 		Assert.assertEquals(list.size() > 0, true);
-		
+
 		driver.navigate().back();
 
 		JavascriptExecutor js = (JavascriptExecutor) driver;
