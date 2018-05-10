ec14f1a7b547184cad2c1257567ca8b7dfddd43b
nareshkumaar <nareshkumar1845@gmail.com>
2018-05-10 15:16:31 -0400

    run.bat update

--- run.bat
+++ run.bat
@@ -1,6 +1,1 @@
-java -cp bin;
-java -cp lib/*; 
-java -cp lib/jcommander-1.72.jar;
-java -cp lib/org.testng.eclipse_6.14.0.201802161500.jar;
-java org.testng.TestNG testng.xml
-
+java -cp bin;lib/* org.testng.TestNG testng.xml
